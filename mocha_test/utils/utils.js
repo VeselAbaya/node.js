25683ed2f7fe7a5e4736201ce5aa6a02436c5421
VeselAbaya <Vandervise465@gmail.com>
2018-10-08 10:17:47 +0300

    how to test async functions is now clear

--- mocha_test/utils/utils.js
+++ mocha_test/utils/utils.js
@@ -1,2 +1,7 @@
 module.exports.add = (a, b) => a + b
 module.exports.square = x => x * x
+module.exports.asyncAdd = (a, b, callback) => {
+  setTimeout(() => {
+    callback(module.exports.add(a, b))
+  }, 1000)
+}
--- mocha_test/utils/utils.test.js
+++ mocha_test/utils/utils.test.js
@@ -4,10 +4,19 @@ it('should add two numbers', () => {
   const res = utils.add(5, 5);
   if (res !== 10)
     throw new Error(`5 + 5 != ${res}`);
-});
+})
 
-it ('should square the number', () => {
-  const res = utils.square(20);
+it('should square the number', () => {
+  const res = utils.square(20)
   if (res !== 400)
-    throw new Error(`20^2 != ${res}`);
-});
+    throw new Error(`20^2 != ${res}`)
+})
+
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(3, 6, (sum) => {
+    if (sum !== 9)
+      throw new Error(`3 + 6 != ${sum}`);
+
+    done();
+  })
+})
